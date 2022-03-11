c9c3cff503c477802ec8dc11ed54e93765e552d7
Maxwell Tolan <96796096+MaxTolan@users.noreply.github.com>
2022-03-11 17:04:42 -0600

    Update cs7350hw2Problem1.py
    Adding part 2

--- cs7350hw2Problem1.py
+++ cs7350hw2Problem1.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Mar 11 14:20:03 2022
 
@@ -88,7 +87,7 @@ class CompleteGraph:
             for k in remainingEdges:
                 self.add_edge(i, k)
 
-    def print_agraph(self):
+    def print_agraph(self): # Need to modify this to fit the desired format
         for i in range(self.V):
             print("Vertex " + str(i) + ":", end="")
             temp = self.graph[i]
@@ -96,6 +95,38 @@ class CompleteGraph:
                 print(" -> {}".format(temp.vertex), end="")
                 temp = temp.next
             print(" \n")
+
+class CycleGraph: 
+    def __init__(self, num):
+        self.V = num
+        self.graph = [None] * self.V
+    
+    def add_edge(self, s, d):
+        node = AdjNode(d)
+        node.next = self.graph[s]
+        self.graph[s] = node
+
+        node = AdjNode(s)
+        node.next = self.graph[d]
+        self.graph[d] = node
+    
+    def allEdgesInCycle(self):
+        for i in range(0,self.V):
+            if i >= self.V -1:
+                self.add_edge(0, self.V -1)
+            else:
+                
+                self.add_edge(i, i+1)
+    def print_agraph(self): # Need to modify this to fit the desired format
+        print(self.V)
+        for i in range(self.V):
+            print("Vertex " + str(i) + ":", end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+                print(" \n")       
+            
 if __name__ == "__main__": # Taken from: https://www.programiz.com/dsa/graph-adjacency-list
     
 
@@ -108,19 +139,24 @@ if __name__ == "__main__": # Taken from: https://www.programiz.com/dsa/graph-adj
     # graph.print_agraph()
     runtimeStorage = []
     vStorage = []
-    for V in range(50,300):
+    #
+    
+    for V in range(5): # For complete graph, it looks like n^2
         start = time.time()
-        completeGraph = CompleteGraph(V)
-        completeGraph.all_edges()
+        # completeGraph = CompleteGraph(V)
+        # completeGraph.all_edges()
+        cycle = CycleGraph(V)
+        cycle.allEdgesInCycle()
         end = time.time()
         runtime = end - start
         runtimeStorage.append(runtime)
         vStorage.append(V)
         print(runtime)
     # completeGraph.print_agraph()
-plt.plot(vStorage, runtimeStorage)
-plt.xlabel('Number of Items in Complete Graph')
-plt.ylabel('Run Time (seconds)')
-plt.title('Run Time vs Number of Items')
+    cycle.print_agraph()
+    plt.plot(vStorage, runtimeStorage)
+    plt.xlabel('Number of Items in Complete Graph')
+    plt.ylabel('Run Time (seconds)')
+    plt.title('Run Time vs Number of Items')
 
     
