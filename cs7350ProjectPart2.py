f6936ec74c978198f2d8fa60e142afd28679726f
Maxwell Tolan <96796096+MaxTolan@users.noreply.github.com>
2022-04-20 19:51:11 -0500

    Update cs7350ProjectPart2.py

--- cs7350ProjectPart2.py
+++ cs7350ProjectPart2.py
@@ -4,54 +4,57 @@ Created on Tue Apr 19 18:01:32 2022
 
 @author: Max
 """
+from numpy import number
+
+
 class SLVO:
     def __init__(self, adjacencyList):
         self.List = adjacencyList
-    
    
-    def adjacencyListStringToArrays(self): 
+    def adjacencyListStringToDict(self): 
         listAsList = self.List.split('\t')
         numberVertices = int(listAsList[0])
         startPoints = listAsList[1 : numberVertices+1]
         edgesDict = {}
-        # edges = []
-        # edgeQuantity = []
         edgeQuantityDict = {}
         for i in range(numberVertices -1) : 
-            # edges.append(listAsList[int(startPoints[i]) : int(startPoints[i+1])])
             edgesDict[i] = listAsList[int(startPoints[i]) : int(startPoints[i+1])]
-            # edgeQuantity.append(len(listAsList[int(startPoints[i]) : int(startPoints[i+1])]))
             edgeQuantityDict[i] = len(listAsList[int(startPoints[i]) : int(startPoints[i+1])])
-        # edges.append(listAsList[ int(startPoints[-1] )-1 :-1])
         edgesDict[numberVertices-1] = listAsList[ int(startPoints[-1] )-1 :-1]
         edgeQuantityDict[numberVertices -1] = len(listAsList[ int(startPoints[-1] )-1 :-1])
-        # edgeQuantity.append(len(listAsList[int(startPoints[-1])-1 : -1]))
         print(edgesDict)
-        print(edgeQuantityDict)
-        
-        return edgesDict, edgeQuantityDict
+        return edgesDict, edgeQuantityDict, numberVertices
+
+    def minValueFromDict(self, d):
+        minValue = min(d.values())
+        minList = [key for key, value in d.items() if value == minValue]
+        return minList[0] # For multiple acceptable items, just pick the first one 
+
+    def removeMin(self, edgeList, edgeQuantity):
+        keyForMinValue = self.minValueFromDict(edgeQuantity)
+        smallestVertexEdges = edgeList.pop(keyForMinValue)
+        edgeQuantity.pop(keyForMinValue) # Need to update edgeQuantity for next iteration
+
+        for i in smallestVertexEdges:
+            vertexEdge = edgeList[int(i)]
+            # Is there a way for me to add 1 to the color value each time it is called? 
+            vertexEdge.remove(str(keyForMinValue))
+        return edgeList, edgeQuantity, keyForMinValue
     
-    def getMinValue(self, inputlist):
-        minValue = min(inputlist)
-        minIndex=inputlist.index(minValue)
-        return minIndex
-    
-    # TODO: Switch it so that it takes a dictionary instead
-    def removeMin(self, edgeList, edgeQuantity, vertexList):
-        minValue = self.getMinValue(edgeQuantity)
-        # TODO: Figure out how to get the original index of the list. If I insert min value, then I risk duplicate indexes
-        edgeQuantity.clear()
-        smallestVertex = edges.pop(minValue)
-        for i in smallestVertex:
-            if int(i) > minValue:
-                connectedVertex = edges[int(i)-1]
-            else:
-                connectedVertex = edges[int(i)]
-            connectedVertex.remove(str(minValue))
+    def organizeOutput(self):
+        edges, edgeQuantity, numberVertices = self.adjacencyListStringToDict()
+        originalEdges = edges
+        keyOrder = []
+        finalColor = {}
+        currentColor = 0
+        for i in range(numberVertices):
+            edges, edgeQuantity, keyForMinValue = self.removeMin(edges, edgeQuantity)
+            finalColor[keyForMinValue] = currentColor
+            currentColor += 1
+            keyOrder.insert(0, keyForMinValue)
+            
+        return keyOrder, finalColor
         
-        for i in range(len(edges)):
-            edgeQuantity.append(len(edges[i]))
-        return edgeList, edgeQuantity, vertexList
 if __name__ == "__main__": 
 
     testCase1 = '5	6	9	11	15	19	3	2	4	3	2	0	1	3	4	0	1	4	2	3	0	2'
@@ -61,10 +64,11 @@ if __name__ == "__main__":
     testCase5 = '5	6	10	14	18	22	4	3	2	1	4	3	2	0	4	3	1	0	4	2	1	0	3	2	1	0'
     
     slvo = SLVO(testCase1)
-    edges, edgeQuantity = slvo.adjacencyListStringToArrays()
-
-    edges, edgeQuantity, vertexList = slvo.removeMin(edges, edgeQuantity, [])
-    print(edges)
-    print(edgeQuantity)
+    keyorder, finalcolor = slvo.organizeOutput()
+    print(keyorder)
+    print(finalcolor)
+    
+    # print(edges)
+    # print(edgeQuantity)
     # print(edges)
     # print(edgeQuantity)
