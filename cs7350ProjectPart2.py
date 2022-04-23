205c6a74e86affcf3be777abdd0c0a93f39f0bb5
Maxwell Tolan <96796096+MaxTolan@users.noreply.github.com>
2022-04-23 15:42:29 -0500

    Update cs7350ProjectPart2.py

--- cs7350ProjectPart2.py
+++ cs7350ProjectPart2.py
@@ -111,7 +111,127 @@ class RandomOrder:
         array.remove(-1)
         return array
     
+class BiggestLastVertexOrdering:
+    def __init__(self, adjacencyList):
+        self.List = adjacencyList
+   
+    def adjacencyListStringToDict(self): 
+        listAsList = self.List.split('\t')
+        numberVertices = int(listAsList[0])
+        startPoints = listAsList[1 : numberVertices+1]
+        edgesDict = {}
+        edgeQuantityDict = {}
+        for i in range(numberVertices -1) : 
+            edgesDict[i] = listAsList[int(startPoints[i]) : int(startPoints[i+1])]
+            edgeQuantityDict[i] = len(listAsList[int(startPoints[i]) : int(startPoints[i+1])])
+        edgesDict[numberVertices-1] = listAsList[ int(startPoints[-1] )-1 :-1]
+        edgeQuantityDict[numberVertices -1] = len(listAsList[ int(startPoints[-1] )-1 :-1])
+        # print(edgesDict)
+        return edgesDict, edgeQuantityDict, numberVertices
+
+    def maxValueFromDict(self, d):
+        maxValue = max(d.values())
+        maxList = [key for key, value in d.items() if value == maxValue]
+        return maxList[0] # For multiple acceptable items, just pick the first one 
+
+    def removeMax(self, edgeList, edgeQuantity):
+        keyForMaxValue = self.maxValueFromDict(edgeQuantity)
+        largestVertexEdges = edgeList.pop(keyForMaxValue)
+        edgeQuantity.pop(keyForMaxValue) # Need to update edgeQuantity for next iteration
+
+        for i in largestVertexEdges:
+            vertexEdge = edgeList[int(i)]
+            # Is there a way for me to add 1 to the color value each time it is called? 
+            vertexEdge.remove(str(keyForMaxValue))
+        return edgeList, edgeQuantity, keyForMaxValue
     
+    def organizeOutput(self):
+        edges, edgeQuantity, numberVertices = self.adjacencyListStringToDict()
+        originalEdges = edges
+        keyOrder = []
+        finalColor = {}
+        currentColor = 0
+        for i in range(numberVertices):
+            edges, edgeQuantity, keyForMinValue = self.removeMin(edges, edgeQuantity)
+            finalColor[keyForMinValue] = currentColor
+            currentColor += 1
+            keyOrder.insert(0, keyForMinValue)
+            
+        return keyOrder, finalColor
+class BiggestOriginalDegreeLast:
+    def __init__(self, adjacencyList):
+        self.List = adjacencyList
+   
+    def adjacencyListStringToDictAndArray(self): 
+        listAsList = self.List.split('\t')
+        numberVertices = int(listAsList[0])
+        startPoints = listAsList[1 : numberVertices+1]
+        edges = []
+        edgeQuantity = []
+        edgesDict = {}
+        edgeQuantityDict = {}
+        for i in range(numberVertices -1) : 
+            edges.append(listAsList[int(startPoints[i]) : int(startPoints[i+1])])
+            edgeQuantity.append(len(listAsList[int(startPoints[i]) : int(startPoints[i+1])]))
+            edgesDict[i] = listAsList[int(startPoints[i]) : int(startPoints[i+1])]
+            edgeQuantityDict[i] = len(listAsList[int(startPoints[i]) : int(startPoints[i+1])])
+        edgesDict[numberVertices-1] = listAsList[ int(startPoints[-1] )-1 :-1]
+        edgeQuantityDict[numberVertices -1] = len(listAsList[ int(startPoints[-1] )-1 :-1])
+        # print(edgesDict)
+        return edgesDict, edgeQuantityDict, numberVertices, edges, edgeQuantity
+    
+    def dictionaryKeysToSortedArray(self, d):
+        array = []
+        for k in sorted(d, key=lambda k: len(d[k]), reverse=False):
+            array.append(k)
+        return array
+class VerticesOrganizedAlphabetically:
+    def __init__(self, adjacencyList):
+        self.List = adjacencyList
+
+"""Given an int32 number, print it in English."""
+def int_to_en(num): # Taken from https://stackoverflow.com/questions/8982163/how-do-i-tell-python-to-convert-integers-into-words
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten',
+          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen',
+          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen',
+          19 : 'nineteen', 20 : 'twenty',
+          30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty',
+          70 : 'seventy', 80 : 'eighty', 90 : 'ninety' }
+    k = 1000
+    m = k * 1000
+    b = m * 1000
+    t = b * 1000
+
+    assert(0 <= num)
+
+    if (num < 20):
+        return d[num]
+
+    if (num < 100):
+        if num % 10 == 0: return d[num]
+        else: return d[num // 10 * 10] + '-' + d[num % 10]
+
+    if (num < k):
+        if num % 100 == 0: return d[num // 100] + ' hundred'
+        else: return d[num // 100] + ' hundred and ' + int_to_en(num % 100)
+
+    if (num < m):
+        if num % k == 0: return int_to_en(num // k) + ' thousand'
+        else: return int_to_en(num // k) + ' thousand, ' + int_to_en(num % k)
+
+    if (num < b):
+        if (num % m) == 0: return int_to_en(num // m) + ' million'
+        else: return int_to_en(num // m) + ' million, ' + int_to_en(num % m)
+
+    if (num < t):
+        if (num % b) == 0: return int_to_en(num // b) + ' billion'
+        else: return int_to_en(num // b) + ' billion, ' + int_to_en(num % b)
+
+    if (num % t == 0): return int_to_en(num // t) + ' trillion'
+    else: return int_to_en(num // t) + ' trillion, ' + int_to_en(num % t)
+
+    raise AssertionError('num is too large: %s' % str(num))
     
 if __name__ == "__main__": 
 
@@ -131,6 +251,10 @@ if __name__ == "__main__":
     randomOrder = RandomOrder(testCase1)
     edgesDict, edgeQuantityDict, numberVertices, edges, edgeQuantity = randomOrder.adjacencyListStringToDictAndArray()
     print(randomOrder.takeKeysFromDictAndPutInArrayRandomly(edgesDict))
+
+    biggestLastVertexOrder = BiggestLastVertexOrdering(testCase1)
+    keyOrderBLVO, finalcolor = biggestLastVertexOrder.organizeOutput()
+    print(keyOrderBLVO)
     # print(edges)
     # print(edgeQuantity)
     # print(edges)
